28a1a73eab57e7a48d63fbc68046e73df8f54eba
Bernardo de Macedo Barbosa <b.macedo@academico.ifrn.edu.br>
2022-01-04 07:53:51 -0300

    teste

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World no Git</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Bernardo de Macedo</h1>
+    <h1 class="text-center">Bernardo de Macedo</h1>
 </body>
 </html>
